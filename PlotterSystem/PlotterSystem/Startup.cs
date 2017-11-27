bef5c5600b3aabdc674a512ad839bed929811b48
DESKTOP-17AN034\sopor <maperez@onepor.com.mx>
2017-11-27 09:01:26 -0600

    version 0.01

---
+++ PlotterSystem/PlotterSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlotterSystem.Startup))]
+namespace PlotterSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
